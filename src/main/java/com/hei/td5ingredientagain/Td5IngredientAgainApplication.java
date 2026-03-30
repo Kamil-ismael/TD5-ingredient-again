6f660673c1df441e4def3d1eabe4cbc30cd97abc
Ismael <hei.ismael.3@gmail.com>
2026-03-30 14:33:45 +0300

    chore: add config datasource

---
+++ src/main/java/com/hei/td5ingredientagain/Td5IngredientAgainApplication.java
@@ -0,0 +1,13 @@
+package com.hei.td5ingredientagain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Td5IngredientAgainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Td5IngredientAgainApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hei/td5ingredientagain/Td5IngredientAgainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hei.td5ingredientagain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Td5IngredientAgainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
